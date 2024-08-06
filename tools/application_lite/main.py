48c86f7a3c591836f85e3f2e89c1593727b42775
John Abanes <john.abanes@nyu.edu>
2024-08-06 14:55:27 -0400

    update submodules

--- tools/application_lite/main.py
+++ tools/application_lite/main.py
@@ -2,12 +2,17 @@ from PySide6.QtWidgets import QApplication
 from mainwindow import MainWindow
 import sys
 import os
+import qdarktheme
 
 if sys.platform == "linux":
     # Fixes "undefined symbol: wl_proxy_marshal_flags": https://bugreports.qt.io/browse/QTBUG-114635}}
     os.environ.setdefault("QT_QPA_PLATFORM", "xcb")
 
 app = QApplication(sys.argv)
+app.setStyleSheet(qdarktheme.load_stylesheet())
+stylesheet = qdarktheme.load_stylesheet('light')
+QApplication.instance().setStyleSheet(stylesheet)
+
 w = MainWindow(app)
 w.show()
 app.exec()
